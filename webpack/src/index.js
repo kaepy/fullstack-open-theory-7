2731eee691a7e353fd2caff7ec898f59f426454c
Henna Tuunainen <henna.tuunainen@gmail.com>
2026-01-04 17:23:16 +0200

    Webpack: Sourcemappaus

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,5 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
