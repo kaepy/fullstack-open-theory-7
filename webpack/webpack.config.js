d87eb243df8f7adce9dfa044b473704971ba6441
Henna Tuunainen <henna.tuunainen@gmail.com>
2026-01-04 13:58:11 +0200

    Webpack: Konfiguraatiotiedosto

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,11 +1,11 @@
-const path = require("path");
+const path = require("path"); // import path module
 
 const config = () => {
   return {
-    entry: "./src/index.js",
+    entry: "./src/index.js", // relative path is okay
     output: {
-      path: path.resolve(__dirname, "build"),
-      filename: "main.js",
+      path: path.resolve(__dirname, "build"), // absolute path is required
+      filename: "main.js", // output file name
     },
   };
 };
