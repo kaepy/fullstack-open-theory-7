d3d91f112f7ab92aabb327239669a391a3220417
Henna Tuunainen <henna.tuunainen@gmail.com>
2026-01-07 11:51:07 +0200

    Luokkakomponentit: Luokkakomponentit vs funktionaaliset komponetit

---
+++ class-components/src/App.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props); // Initialize state in the constructor
+
+    this.state = {
+      anecdotes: [],
+      current: 0,
+    };
+  }
+
+  componentDidMount = () => {
+    axios.get("http://localhost:3001/anecdotes").then((response) => {
+      this.setState({ anecdotes: response.data });
+    });
+  };
+
+  handleClick = () => {
+    const current = Math.floor(
+      Math.random() * (this.state.anecdotes.length - 1)
+    );
+    this.setState({ current });
+  };
+
+  render() {
+    if (this.state.anecdotes.length === 0) {
+      return <div>no anecdotes...</div>;
+    }
+
+    return (
+      <div>
+        <h1>anecdote of the day</h1>
+        <div>{this.state.anecdotes[this.state.current].content}</div>
+        <button onClick={this.handleClick}>next</button>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+/* Functional Component Version of the same App
+
+const App = () => {
+  const [anecdotes, setAnecdotes] = useState([])
+  const [current, setCurrent] = useState(0)
+
+  useEffect(() =>{
+    axios.get('http://localhost:3001/anecdotes').then(response => {
+      setAnecdotes(response.data)
+    })
+  },[])
+
+  const handleClick = () => {
+    setCurrent(Math.round(Math.random() * (anecdotes.length - 1)))
+  }
+
+  if (anecdotes.length === 0) {
+    return <div>no anecdotes...</div>
+  }
+
+  return (
+    <div>
+      <h1>anecdote of the day</h1>
+      <div>{anecdotes[current].content}</div>
+      <button onClick={handleClick}>next</button>
+    </div>
+  )
+}
+
+*/
